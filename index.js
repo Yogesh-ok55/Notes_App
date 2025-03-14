b55a1f7e0e3ecacd5890d635971ce33581280455
Yogesh Choudhary <127961738+Yogesh-ok55@users.noreply.github.com>
2025-03-14 23:30:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.static(path.join(__dirname,'public')))
 
 
 app.set('view engine', 'ejs')
+app.set("views", path.join(__dirname, "views"));
 
 app.get('/',function(req,res){
     fs.readdir('./files',(err,files)=>{
